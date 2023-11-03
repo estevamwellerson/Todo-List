c8cee334e979cbd72300981405e4f25ab1aa1ab7
Marcos Rodrigues <eu@marcosrodrigues.me>
2023-11-03 13:04:39 -0300

    fix: remove componente desnecessário
    elementos `ol` e `ul` só devem ter um `li` como filho direto

--- src/pages/home/components/todo-list/todo-list.jsx
+++ src/pages/home/components/todo-list/todo-list.jsx
@@ -5,15 +5,13 @@ const TodoList = ({ items = [], onToggle, onDelete }) => {
     <ul>
       {items.map((item) => {
         return (
-          <div className="list" key={item.id}>
-            <li key={item.id}>
-              <Button className="btn-icon" onClick={() => onToggle(item.id)}>✅</Button>
-              <span style={{textDecoration: item.complete ? 'line-through' : 'none'}}>
-                {item.value}
-              </span>
-              <Button className="btn-icon" onClick={() => onDelete(item.id)} >🗑️</Button>
-            </li>
-          </div>
+          <li key={item.id}>
+            <Button className="btn-icon" onClick={() => onToggle(item.id)}>✅</Button>
+            <span style={{textDecoration: item.complete ? 'line-through' : 'none'}}>
+              {item.value}
+            </span>
+            <Button className="btn-icon" onClick={() => onDelete(item.id)} >🗑️</Button>
+          </li>
         );
       })}
     </ul>
